ade00990f05d5de2a2ced97fd3fc2232e35022d9
Alexius25 <171688835+Alexius25@users.noreply.github.com>
2026-05-02 21:55:26 +0200

    Enabled docker build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextIntlPlugin = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {output: 'standalone'};
 
 export default nextIntlPlugin(nextConfig);
